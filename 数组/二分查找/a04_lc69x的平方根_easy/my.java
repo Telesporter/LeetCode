fc21b1255121d1e4e99b527e9d89f31e3607c8ce
Telesporter <51327163+Telesporter@users.noreply.github.com>
2022-04-01 22:00:11 +0800

    Add files via upload

---
+++ 数组/二分查找/a04_lc69x的平方根_easy/my.java
@@ -0,0 +1,31 @@
+package 数组.二分查找.a04_lc69x的平方根_easy;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 13498
+ * Date: 2022/3/31
+ * Time: 21:41
+ * Description: No Description
+ */
+public class my {
+}
+
+class Solution {
+    public int mySqrt(int x) {
+        int left = 1;
+        int right = x;
+        int leftboard = 0;
+        while(left <= right){
+            int mid = left+((right-left)>>1);
+            if ((long)mid*mid > x){
+                right = mid - 1;
+                leftboard = mid-1;
+            }else if ((long)mid*mid < x){
+                left = mid + 1;
+            }else {
+                return mid;
+            }
+        }
+        return leftboard;
+    }
+}
